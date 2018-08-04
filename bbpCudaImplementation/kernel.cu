c478365a1292d524a0b78e0faddba214befed4b3
Michael Gilbert <euphoricpoptarts@gmail.com>
2018-08-04 03:46:24 -0400

    tweaked number of blocks to maximize performance on Titan X (Pascal); changed method to find highest bit in multiplier to method found on hacker's delight; changed condition for modulus operation in modExpLeftToRight to happen much more often (reduces runtime); added progress tracking, including a separate thread to check and output progress to console; probably some other miscellaneous changes I forgot about; on Titan X (Pascal) 1E11 takes 3680 seconds to calculate.

--- bbpCudaImplementation/kernel.cu
+++ bbpCudaImplementation/kernel.cu
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
+#include <Windows.h>
 
 #define TYPE unsigned long long
 #define INT_64 unsigned long long
@@ -14,15 +15,24 @@ struct sJ {
 	double s1, s4, s5, s6;
 };
 
+typedef struct progressData {
+	volatile INT_64 *currentProgress;
+	TYPE maxProgress;
+	int quit = 0;
+} PROGRESSDATA, *PPROGRESSDATA;
+
 cudaError_t addWithCuda(sJ *c, unsigned int size, TYPE digit);
 
-const int threadCountPerBlock = 32;
-const int blockCount = 256;
+//warpsize is 32 so optimal value is probably always a multiple of 32
+const int threadCountPerBlock = 64;
+//this is more difficult to optimize but seems to not like odd numbers
+const int blockCount = 560;
 
 __device__ const TYPE baseSystem = 16;
 __device__ const int baseExpOf2 = 4;
 
 __device__ const int typeSize = sizeof(TYPE) * 8 - 1;
+__device__ const TYPE multiplyModCond = 0x2000000000000000;//2^61
 __device__ const int int64Size = sizeof(INT_64) * 8 - 1;
 __device__ const INT_64 int64ModCond = 0x40000000;
 __device__ const INT_64 int64MaxBit = 0x8000000000000000;
@@ -90,19 +100,42 @@ __device__ void findMultiplierHighestBit(const TYPE multiplier, TYPE *output) {
 	*output = highestBit;
 }
 
+//hacker's delight method to find highest bit in a long long (it just works)
+//http://graphics.stanford.edu/~seander/bithacks.html
+//just barely faster than built-in CUDA __clzll
+__device__ void findMultiplierHighestBitHackersDelight(TYPE multiplier, TYPE *output) {
+	
+	multiplier |= multiplier >> 1;
+	multiplier |= multiplier >> 2;
+	multiplier |= multiplier >> 4;
+	multiplier |= multiplier >> 8;
+	multiplier |= multiplier >> 16;
+	multiplier |= multiplier >> 32;
+
+	*output = multiplier ^ (multiplier >> 1);
+
+}
+
 __device__ void modMultiplyLeftToRight(const TYPE multiplicand, const TYPE multiplier, TYPE mod, TYPE *output) {
 	TYPE result = multiplicand;
 
-	//only perform modulus operation during loop if result is >= (TYPE maximum + 1)/8 (in order to prevent overflowing)
-	TYPE modCond = 1L;
-	modCond <<= (typeSize - 2);//2^61
+	//TYPE highestBitMask = 0;
+
+	//findMultiplierHighestBit(multiplier, &highestBitMask);
+
+	/*unsigned long zeroCount = 0;
+
+	zeroCount = __clzll(multiplier);*/
 
 	TYPE highestBitMask = 0;
 
-	findMultiplierHighestBit(multiplier, &highestBitMask);
+	//highestBitMask <<= (63 - zeroCount);
+	
+	findMultiplierHighestBitHackersDelight(multiplier, &highestBitMask);
 
 	while (highestBitMask > 1) {
-		if (result >= modCond) result %= mod;
+		//only perform modulus operation during loop if result is >= (TYPE maximum + 1)/8 (in order to prevent overflowing)
+		if (result >= multiplyModCond) result %= mod;
 		result <<= 1;
 		highestBitMask >>= 1;
 		if (multiplier&highestBitMask)	result += multiplicand;
@@ -151,7 +184,11 @@ __device__ void modExpLeftToRight(const TYPE exp, TYPE mod, TYPE highestBitMask,
 	INT_64 modCond = int64ModCond;
 
 	while (highestBitMask > 1) {
-		if (result >= modCond) result %= mod;//quickMod(result, mod, &result);
+		//this is not necessary as modMultiplyLeftToRight ensures result never overflows a 64 bit buffer
+		//however performing this modulus saves time (more is less)
+		//likely saves performing some moduli in modMultiplyLeftToRight or reduces overall size of arguments
+		if (result >= mod) result %= mod;//quickMod(result, mod, &result);
+
 		modMultiplyLeftToRight(result, result, mod, &result);//result *= result;
 		highestBitMask >>= 1;
 		if (exp&highestBitMask)	result <<= baseExpOf2;//modMultiplyLeftToRight(result, base, mod, &result);//result *= base;
@@ -171,7 +208,7 @@ __device__ void fractionalPartOfSum(TYPE exp, TYPE mod, double *partialSum, TYPE
 
 //stride over all parts of summation in bbp formula where k <= n
 //to compute partial sJ sums
-__device__ void bbp(TYPE n, TYPE start, TYPE stride, sJ *output) {
+__device__ void bbp(TYPE n, TYPE start, TYPE stride, sJ *output, volatile INT_64 *progress) {
 
 	double s1 = 0.0, s4 = 0.0, s5 = 0.0, s6 = 0.0;
 	double trash = 0.0;
@@ -192,6 +229,10 @@ __device__ void bbp(TYPE n, TYPE start, TYPE stride, sJ *output) {
 		s4 = modf(s4, &trash);
 		s5 = modf(s5, &trash);
 		s6 = modf(s6, &trash);
+		if (start == 0) {
+			//only 1 thread ever updates the progress
+			*progress = k;
+		}
 	}
 	output[start].s1 = s1;
 	output[start].s4 = s4;
@@ -201,11 +242,11 @@ __device__ void bbp(TYPE n, TYPE start, TYPE stride, sJ *output) {
 
 //determine from thread and block position where to begin stride
 //and how wide stride is
-__global__ void bbpKernel(sJ *c, TYPE digit)
+__global__ void bbpKernel(sJ *c, volatile INT_64 *progress, TYPE digit)
 {
 	TYPE stride = blockDim.x * gridDim.x;
 	TYPE i = threadIdx.x + blockDim.x * blockIdx.x;
-	bbp(digit, i, stride, c);
+	bbp(digit, i, stride, c, progress);
 }
 
 //stride over current leaves of reduce tree
@@ -289,35 +330,61 @@ long finalizeDigit(sJ input, TYPE n) {
 
 int main()
 {
-	const int arraySize = threadCountPerBlock * blockCount;
-	const TYPE digitPosition = 99999999999;
-	sJ c[arraySize];
+	try {
+		const int arraySize = threadCountPerBlock * blockCount;
+		const TYPE digitPosition = 9999999999;
+		sJ* c = new sJ[arraySize];
 
-	clock_t start = clock();
+		clock_t start = clock();
 
-	cudaError_t cudaStatus = addWithCuda(c, arraySize, digitPosition);
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "addWithCuda failed!");
+		cudaError_t cudaStatus = addWithCuda(c, arraySize, digitPosition);
+		if (cudaStatus != cudaSuccess) {
+			fprintf(stderr, "addWithCuda failed!");
+			return 1;
+		}
+
+		long hexDigit = finalizeDigit(c[0], digitPosition);
+
+		clock_t end = clock();
+
+		printf("pi at hexadecimal digit %llu is %X\n",
+			digitPosition + 1, hexDigit);
+
+		printf("Computed in %.8f seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+
+		// cudaDeviceReset must be called before exiting in order for profiling and
+		// tracing tools such as Nsight and Visual Profiler to show complete traces.
+		cudaStatus = cudaDeviceReset();
+		if (cudaStatus != cudaSuccess) {
+			fprintf(stderr, "cudaDeviceReset failed!");
+			return 1;
+		}
+
+		return 0;
+	}
+	catch(...) {
+		printf("oops xD\n");
 		return 1;
 	}
+}
 
-	long hexDigit = finalizeDigit(c[0], digitPosition);
+//this function is meant to be run by an independent thread to output progress to the console
+DWORD WINAPI progressCheck(LPVOID data) {
+	PPROGRESSDATA progP = (PPROGRESSDATA)data;
 
-	clock_t end = clock();
+	double lastProgress = 0;
 
-	printf("pi at hexadecimal digit %llu is %X\n",
-		digitPosition + 1, hexDigit);
+	while(!(*progP).quit) {
+		double progress = (double)(*((*progP).currentProgress)) / (double)(*progP).maxProgress;
 
-	printf("Computed in %.8f seconds\n", (double)(end - start) / CLOCKS_PER_SEC);
+		if (progress - lastProgress > 1E-4) {
+			double timeEst = (1.0 - progress)*0.1 / (progress - lastProgress);
+			lastProgress = progress;
+			printf("Current progress is %3.3f%%. Estimated total runtime remaining is %8.3f seconds.\n", 100.0*progress, timeEst);
+		}
 
-	// cudaDeviceReset must be called before exiting in order for profiling and
-	// tracing tools such as Nsight and Visual Profiler to show complete traces.
-	cudaStatus = cudaDeviceReset();
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaDeviceReset failed!");
-		return 1;
+		Sleep(100);
 	}
-
 	return 0;
 }
 
@@ -325,12 +392,23 @@ int main()
 cudaError_t addWithCuda(sJ *c, unsigned int size, TYPE digit)
 {
 	sJ *dev_c = 0;
+
+	//these variables are linked between host and device memory allowing each to communicate about progress
+	volatile INT_64 *currProgHost, *currProgDevice;
+
 	cudaError_t cudaStatus;
 
 	// Choose which GPU to run on, change this on a multi-GPU system.
 	cudaStatus = cudaSetDevice(0);
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+		goto Error;
+	}
+
+	//allow device to map host memory for progress ticker
+	cudaSetDeviceFlags(cudaDeviceMapHost);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaSetDeviceFlags failed!");
 		goto Error;
 	}
 
@@ -341,8 +419,33 @@ cudaError_t addWithCuda(sJ *c, unsigned int size, TYPE digit)
 		goto Error;
 	}
 
+	// Allocate Host memory for progress ticker
+	cudaStatus = cudaHostAlloc((void**)&currProgHost, sizeof(INT_64), cudaHostAllocMapped);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaHostAalloc failed!");
+		goto Error;
+	}
+
+	//create link between between host and device memory for progress ticker
+	cudaStatus = cudaHostGetDevicePointer((INT_64 **)&currProgDevice, (INT_64 *)currProgHost, 0);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaHostGetDevicePointer failed!");
+		goto Error;
+	}
+
+	*currProgHost = 0;
+
+	PROGRESSDATA threadData = { currProgHost, digit, 0 };
+
+	HANDLE thread = CreateThread(NULL, 0, *progressCheck, (LPVOID) &threadData, 0, NULL);
+
+	if (thread == NULL) {
+		fprintf(stderr, "progressCheck thread creation failed\n");
+		goto Error;
+	}
+
 	// Launch a kernel on the GPU with one thread for each element.
-	bbpKernel << <blockCount, threadCountPerBlock >> >(dev_c, digit);
+	bbpKernel << <blockCount, threadCountPerBlock >> >(dev_c, currProgDevice, digit);
 
 	// Check for any errors launching the kernel
 	cudaStatus = cudaGetLastError();
@@ -359,26 +462,11 @@ cudaError_t addWithCuda(sJ *c, unsigned int size, TYPE digit)
 		goto Error;
 	}
 
-	// Copy output vector from GPU buffer to host memory.
-	cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(sJ), cudaMemcpyDeviceToHost);
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMemcpy failed!");
-		goto Error;
-	}
+	//tell the progress thread to quit
+	threadData.quit = 1;
 
-	sJ expected;
-
-	expected.s1 = 0;
-	expected.s4 = 0;
-	expected.s5 = 0;
-	expected.s6 = 0;
-
-	for (int j = 0; j < size; j++) {
-		expected.s1 += c[j].s1;
-		expected.s4 += c[j].s4;
-		expected.s5 += c[j].s5;
-		expected.s6 += c[j].s6;
-	}
+	WaitForSingleObject(thread, INFINITE);
+	CloseHandle(thread);
 
 	cudaStatus = reduceSJ(dev_c, size);
 
@@ -386,28 +474,11 @@ cudaError_t addWithCuda(sJ *c, unsigned int size, TYPE digit)
 		goto Error;
 	}
 
-	// Copy output vector from GPU buffer to host memory.
+	// Copy result vector from GPU buffer to host memory.
 	cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(sJ), cudaMemcpyDeviceToHost);
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaMemcpy failed!");
 		goto Error;
-	}
-
-	if (fabs(c[0].s1 - expected.s1) > 1e-10) {
-		printf("s1 not correct\n");
-		printf("Expected %.8f; Actual %.8f\n", expected.s1, c[0].s1);
-	}
-	if (fabs(c[0].s4 - expected.s4) > 1e-10) {
-		printf("s4 not correct\n");
-		printf("Expected %.8f; Actual %.8f\n", expected.s4, c[0].s4);
-	}
-	if (fabs(c[0].s5 - expected.s5) > 1e-10) {
-		printf("s5 not correct\n");
-		printf("Expected %.8f; Actual %.8f\n", expected.s5, c[0].s5);
-	}
-	if (fabs(c[0].s6 - expected.s6) > 1e-10) {
-		printf("s6 not correct\n");
-		printf("Expected %.8f; Actual %.8f\n", expected.s6, c[0].s6);
 	}
 
 Error:
